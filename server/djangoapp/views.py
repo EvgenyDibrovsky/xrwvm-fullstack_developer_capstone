4eebd5d5a8f950e35b365c8aa329bb22a8ff0129
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-03-15 09:55:33 +0100

    commit

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -68,7 +68,7 @@ def registration(request):
 
     # If it is a new user
     if not username_exist:
-        # Create user in auth_user table
+    # Create user in auth_user table
         user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
@@ -101,6 +101,7 @@ def get_cars(request):
     print(count)
     if(count == 0):
         initiate()
+
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
